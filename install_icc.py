1bd48c9396c7dc6700f35b19bdd0f427c9877a45
hsc <hsc@hsc.hsc>
2016-02-25 03:10:56 +0100

    Fixed icc install.

--- install_icc.py
+++ install_icc.py
@@ -7,7 +7,7 @@ import os
 if __name__ == "__main__":
     if platform.system() == "Linux" or platform.system() == "Darwin":
         ICC_DEST = os.environ.get("HOME")+"/ci-tools/icc"
-        os.system("sudo /bin/sh `pwd`/ci/install_icc.sh --dest "+ICC_DEST)
+        os.system("sudo /bin/sh `pwd`/ci-tools/install_icc.sh --dest "+ICC_DEST)
     elif platform.system() == "Windows":
         ICC_DEST = os.getenv("TMP")+"/ci-tools/icc"
         #TODO
