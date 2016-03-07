88a98a7658b8cd6d709f1ab934e2b4000f560683
hsc <kevin.patrick.schmidt@googlemail.com>
2016-03-07 22:19:05 +0100

    Fixed install_icc.

--- install_icc.py
+++ install_icc.py
@@ -7,7 +7,5 @@ import os
 if __name__ == "__main__":
     CITOOLS_PATH = os.path.join(os.getcwd(), "ci-tools")
     ICC_PATH = os.path.join(CITOOLS_PATH, "icc")
-    if platform.system() == "Linux" or platform.system() == "Darwin":
+    if platform.system() == "Linux":
         os.system("sudo /bin/sh "+os.path.join(CITOOLS_PATH, "install_icc.sh")+" --dest "+ICC_PATH)
-    elif platform.system() == "Windows":
-        #TODO
