ccd5f8c46ec5ca3dae0495fc200b3ebd26c4899d
hsc <kevin.patrick.schmidt@googlemail.com>
2016-03-13 03:29:29 +0100

    Added fallback to system cmake.

--- run_ctest.py
+++ run_ctest.py
@@ -9,14 +9,15 @@ if __name__ == "__main__":
     CITOOLS_PATH = os.path.join(os.getcwd(), "ci-tools")
     CMAKE_PATH = os.path.join(CITOOLS_PATH, "cmake")
     ICC_PATH = os.path.join(CITOOLS_PATH, "icc")
+
     if platform.system() == "Linux":
-        CTEST = os.path.join(CMAKE_PATH, "bin", "ctest")
+        os.environ["PATH"] = os.path.join(CMAKE_PATH, "bin")+":"+os.environ.get("PATH", os.path.join(CMAKE_PATH, "bin"))
         if os.path.exists(ICC_PATH):
             os.environ["LD_LIBRARY_PATH"] = os.path.join(ICC_PATH, "ism", "bin", "intel64")+":"+os.path.join(ICC_PATH, "lib", "intel64_lin")
     elif platform.system() == "Windows":
-        CTEST = os.path.join(CMAKE_PATH, "bin", "ctest")
+        os.environ["PATH"] = os.path.join(CMAKE_PATH, "bin")+";"+os.environ.get("PATH", os.path.join(CMAKE_PATH, "bin"))
     elif platform.system() == "Darwin":
-        CTEST = os.path.join(CMAKE_PATH, "CMake.app", "Contents", "bin", "ctest")
+        os.environ["PATH"] = os.path.join(CMAKE_PATH, "CMake.app", "Contents", "bin")+":"+os.environ.get("PATH", os.path.join(CMAKE_PATH, "bin"))
 
-    if subprocess.call(CTEST+" -VV -S ci-tools/run_ctest.cmake", shell=True) != 0:
+    if subprocess.call("ctest -VV -S ci-tools/run_ctest.cmake", shell=True) != 0:
         raise Exception("ctest returned an error.")
