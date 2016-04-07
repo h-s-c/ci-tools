45026614a1c93ebd280c6a7539e15061c3f0468a
hsc <kevin.patrick.schmidt@googlemail.com>
2016-04-07 20:44:20 +0200

    Added cpack script.

--- run_cpack.py
+++ run_cpack.py
@@ -8,16 +8,17 @@ import subprocess
 if __name__ == "__main__":
     CITOOLS_PATH = os.path.join(os.getcwd(), "ci-tools")
     CMAKE_PATH = os.path.join(CITOOLS_PATH, "cmake")
-    ICC_PATH = os.path.join(CITOOLS_PATH, "icc")
 
     if platform.system() == "Linux":
         os.environ["PATH"] = os.path.join(CMAKE_PATH, "bin")+":"+os.environ.get("PATH", os.path.join(CMAKE_PATH, "bin"))
-        if os.path.exists(ICC_PATH):
-            os.environ["LD_LIBRARY_PATH"] = os.path.join(ICC_PATH, "ism", "bin", "intel64")+":"+os.path.join(ICC_PATH, "lib", "intel64_lin")
     elif platform.system() == "Windows":
         os.environ["PATH"] = os.path.join(CMAKE_PATH, "bin")+";"+os.environ.get("PATH", os.path.join(CMAKE_PATH, "bin"))
     elif platform.system() == "Darwin":
         os.environ["PATH"] = os.path.join(CMAKE_PATH, "CMake.app", "Contents", "bin")+":"+os.environ.get("PATH", os.path.join(CMAKE_PATH, "bin"))
 
-    if subprocess.call("ctest -VV -S ci-tools/run_ctest.cmake", shell=True) != 0:
-        raise Exception("CTest returned an error.")
+    if subprocess.call("cmake -DKD_BUILD_TESTS=Off -DKD_BUILD_EXAMPLES=Off -Bbuild -H.", shell=True) != 0:
+        raise Exception("CMake configure returned an error.")
+    if subprocess.call("cmake --build build --config Release", shell=True) != 0:
+        raise Exception("CMake build returned an error.")
+    if subprocess.call("cpack -C Release", shell=True) != 0:
+        raise Exception("CPack returned an error.")
