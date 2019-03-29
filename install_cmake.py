26677f9de14d71709fcd79484462404ba4f3b34c
Kevin Schmidt <kevin.patrick.schmidt@googlemail.com>
2019-03-29 21:46:16 +0100

    Update cmake.

--- install_cmake.py
+++ install_cmake.py
@@ -12,8 +12,8 @@ import shutil
 import hashlib
 import ssl
 
-CMAKE_BASE_URL = "http://cmake.org/files/v3.13/"
-CMAKE_VERSION = "3.13.4"
+CMAKE_BASE_URL = "http://cmake.org/files/v3.14/"
+CMAKE_VERSION = "3.14.1"
 CMAKE_FILENAME_LINUX_32 = "cmake-"+CMAKE_VERSION+"-Linux-i386"
 CMAKE_FILENAME_LINUX_64 = "cmake-"+CMAKE_VERSION+"-Linux-x86_64"
 CMAKE_FILENAME_WINDOWS = "cmake-"+CMAKE_VERSION+"-win32-x86"
