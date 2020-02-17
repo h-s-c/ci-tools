4d19cc36e866c8e21a526cd228f170ffd177292b
Kevin Schmidt <kevin.patrick.schmidt@googlemail.com>
2020-02-17 20:22:37 +0100

    Test travisci windows build workaround. (3)

--- run_ctest.py
+++ run_ctest.py
@@ -4,10 +4,6 @@
 import platform
 import os
 import subprocess
-import sys
-
-if platform.system() == "Windows":
-    import distutils.msvc9compiler as msvc
 
 if __name__ == "__main__":
     CITOOLS_PATH = os.path.join(os.getcwd(), "ci-tools")
@@ -17,14 +13,6 @@ if __name__ == "__main__":
         os.environ["PATH"] = os.path.join(CMAKE_PATH, "bin")+":"+os.environ.get("PATH", os.path.join(CMAKE_PATH, "bin"))
     elif platform.system() == "Windows":
         os.environ["PATH"] = os.path.join(CMAKE_PATH, "bin")+";"+os.environ.get("PATH", os.path.join(CMAKE_PATH, "bin"))
-        if len(sys.argv) > 1:
-            msvc.find_vcvarsall = lambda _: sys.argv[1]
-            envs = msvc.query_vcvarsall(sys.argv[2])
-            for k,v in envs.items():
-                k = k.upper()
-                v = ":".join(subprocess.check_output(["cygpath","-u",p]).rstrip() for p in v.split(";"))
-                v = v.replace("'\''",r"'\'\\\'\''")
-                print "export %(k)s='\''%(v)s'\''" % locals()
     elif platform.system() == "Darwin":
         os.environ["PATH"] = os.path.join(CMAKE_PATH, "CMake.app", "Contents", "bin")+":"+os.environ.get("PATH", os.path.join(CMAKE_PATH, "bin"))
 
