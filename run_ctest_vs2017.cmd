3feae7b69f4c90237d6564176e6071879658a00d
Kevin Schmidt <kevin.patrick.schmidt@googlemail.com>
2020-09-23 15:57:47 +0200

    Add Visual Studio batch scripts to work around travisci limitations.

---
+++ run_ctest_vs2017.cmd
@@ -0,0 +1,2 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" amd64
+python ci-tools/run_ctest.py
---
+++ run_ctest_vs2019.cmd
@@ -0,0 +1,7 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" amd64
+if %ERRORLEVEL% neq 0 (
+    rem Reset %ERRORLEVEL%
+    ver > nul 
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+)
+python ci-tools/run_ctest.py
