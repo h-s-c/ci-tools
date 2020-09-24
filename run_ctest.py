a9d8e523b29af309256a5de299c5c839887581db
Kevin Schmidt <kevin.patrick.schmidt@googlemail.com>
2020-09-24 03:21:11 +0200

    Start persistent wineserver for faster MinGW tests.

--- run_ctest.py
+++ run_ctest.py
@@ -4,11 +4,17 @@
 import platform
 import os
 import subprocess
+import shutil
 
 if __name__ == "__main__":
+    # Start persistent wineserver for faster MinGW tests
+    if platform.system() == "Linux":
+        if shutil.which("wineserver"):
+            subprocess.call("wineserver -p", shell=True)
+
+    # Figure out path of our own cmake install
     CITOOLS_PATH = os.path.join(os.getcwd(), "ci-tools")
     CMAKE_PATH = os.path.join(CITOOLS_PATH, "cmake")
-
     if platform.system() == "Linux":
         os.environ["PATH"] = os.path.join(CMAKE_PATH, "bin")+":"+os.environ.get("PATH", os.path.join(CMAKE_PATH, "bin"))
     elif platform.system() == "Windows":
@@ -16,5 +22,4 @@ if __name__ == "__main__":
     elif platform.system() == "Darwin":
         os.environ["PATH"] = os.path.join(CMAKE_PATH, "CMake.app", "Contents", "bin")+":"+os.environ.get("PATH", os.path.join(CMAKE_PATH, "bin"))
 
-    if subprocess.call("ctest -VV -S ci-tools/run_ctest.cmake", shell=True) != 0:
-        raise Exception("CTest returned an error.")
+    subprocess.call("ctest -VV -S ci-tools/run_ctest.cmake", shell=True, check=True)
