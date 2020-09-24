220badfe73518771f442b11d5482035951b8cc37
Kevin Schmidt <kevin.patrick.schmidt@googlemail.com>
2020-09-24 23:19:12 +0200

    Fix premature script termination.

--- run_ctest_vs2019.cmd
+++ run_ctest_vs2019.cmd
@@ -1,4 +1,4 @@
-refreshenv
+call refreshenv
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" amd64
 if %ERRORLEVEL% neq 0 (
     rem Reset %ERRORLEVEL%
