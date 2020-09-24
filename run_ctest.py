627edb62f1d979d3c7a94e4dcc67a3d7810904c6
Kevin Schmidt <kevin.patrick.schmidt@googlemail.com>
2020-09-24 03:45:20 +0200

    Fix python2 compatibility.

--- run_ctest.py
+++ run_ctest.py
@@ -4,13 +4,11 @@
 import platform
 import os
 import subprocess
-import shutil
 
 if __name__ == "__main__":
     # Start persistent wineserver for faster MinGW tests
     if platform.system() == "Linux":
-        if shutil.which("wineserver"):
-            subprocess.call("wineserver -p", shell=True)
+        subprocess.call("wineserver -p", shell=True)
 
     # Figure out path of our own cmake install
     CITOOLS_PATH = os.path.join(os.getcwd(), "ci-tools")
@@ -22,4 +20,4 @@ if __name__ == "__main__":
     elif platform.system() == "Darwin":
         os.environ["PATH"] = os.path.join(CMAKE_PATH, "CMake.app", "Contents", "bin")+":"+os.environ.get("PATH", os.path.join(CMAKE_PATH, "bin"))
 
-    subprocess.call("ctest -VV -S ci-tools/run_ctest.cmake", shell=True, check=True)
+    subprocess.check_call("ctest -VV -S ci-tools/run_ctest.cmake", shell=True)
