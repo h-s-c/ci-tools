68ec2cc7d9106ac3dc05b4e475b864ac592fa019
Kevin Schmidt <kevin.patrick.schmidt@googlemail.com>
2020-09-25 20:12:25 +0200

    Clean up windows stuff.

--- run_ctest_vs2017.cmd
+++
@@ -1,8 +0,0 @@
-call refreshenv
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" amd64
-if %ERRORLEVEL% neq 0 (
-    rem Reset %ERRORLEVEL%
-    ver > nul 
-    call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
-)
-python ci-tools/run_ctest.py
